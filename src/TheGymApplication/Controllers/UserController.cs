c2f9945fa78a82f3e8732a3c6b8919b70f4a35e8
Patrick Kitchens <patrick.kitchens1@gmail.com>
2021-09-25 19:58:58 -0400

    Added Generic controller with CRUD Added User and user role controller

---
+++ src/TheGymApplication/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheGymApplication.ViewModels;
+using TheGymDomain.Interfaces;
+using TheGymDomain.Models;
+
+namespace TheGymApplication.Controllers
+{
+    /// <summary>
+    /// Uses Generic controller methods for CRUD, can implement custom methods for CRUD with override
+    /// </summary>
+    public class UserController : GenericController<User, UserViewModel>
+    {
+        public UserController(IUnitOfWork unitOfWork, IMapper mapper)
+            : base(unitOfWork, mapper, unitOfWork.UserRepository)
+        {
+
+        }
+    }
+}
