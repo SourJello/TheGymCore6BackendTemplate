facf160b95d13425735472a1b1d8e70e86603404
Patrick Kitchens <patrick.kitchens1@gmail.com>
2021-09-26 18:15:04 -0400

    Changes the default value for createdDate in the base entity to be in UTC because npgsql changed the datatype DateTime maps to to now require the timezone in the value. Breaking change is listed in their v6 release notes
    Added cleans and calls for actually seeding the database
    
    Implements logging to a file

--- src/TheGymDomain/Models/Common/Entity.cs
+++ src/TheGymDomain/Models/Common/Entity.cs
@@ -5,7 +5,7 @@ namespace TheGymDomain.Models.Common
     public abstract class Entity : IEntity
     {
         public Guid Id { get; set; }
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime CreatedDate { get; set; } = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc);
         public DateTime? ModifiedDate { get; set; }
         public Guid CreatedByUserId { get; set; }
         public Guid ModifiedByUserId { get; set; }
