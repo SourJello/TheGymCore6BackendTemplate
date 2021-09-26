b0626c5b7e06e1e610650c058b935671f618e0a7
Patrick Kitchens <patrick.kitchens1@gmail.com>
2021-09-26 14:18:22 -0400

    Changed validator class to not be wrongly nested in the configuration class

--- src/TheGymInfrastructure/Persistence/PostgreSQL/Config/UserConfiguration.cs
+++ src/TheGymInfrastructure/Persistence/PostgreSQL/Config/UserConfiguration.cs
@@ -27,29 +27,28 @@ namespace TheGymInfrastructure.Persistence.PostgreSQL.Config
             builder.Property(x => x.Phone)
                 .IsRequired();
         }
-
-        public class UserValidator : EntityValidator<User>
+    }
+    public class UserValidator : EntityValidator<User>
+    {
+        public UserValidator()
         {
-            public UserValidator()
-            {
-                RuleFor(x => x.FirstName)
-                    .Length(1, 256)
-                    .NotEmpty()
-                    .WithMessage("First name cannot be empty")
-                    .WithErrorCode("First name error");
-                RuleFor(x => x.FirstName)
-                    .Length(1, 256)
-                    .NotEmpty()
-                    .WithMessage("First name cannot be empty")
-                    .WithErrorCode("First name error");
-                RuleFor(x => x.Phone)
-                    .Matches(@"\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$")
-                    .Length(10, 14)
-                    .WithMessage("Phone Format (xxx)-xxx-xxxx, xxx-xxx-xxxx, or xxxxxxxxxx")
-                    .NotEmpty()
-                    .WithMessage("Phone cannot be empty")
-                    .WithErrorCode("Phone error");
-            }
+            RuleFor(x => x.FirstName)
+                .Length(1, 256)
+                .NotEmpty()
+                .WithMessage("First name cannot be empty")
+                .WithErrorCode("First name error");
+            RuleFor(x => x.FirstName)
+                .Length(1, 256)
+                .NotEmpty()
+                .WithMessage("First name cannot be empty")
+                .WithErrorCode("First name error");
+            RuleFor(x => x.Phone)
+                .Matches(@"\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$")
+                .Length(10, 14)
+                .WithMessage("Phone Format (xxx)-xxx-xxxx, xxx-xxx-xxxx, or xxxxxxxxxx")
+                .NotEmpty()
+                .WithMessage("Phone cannot be empty")
+                .WithErrorCode("Phone error");
         }
     }
 }
